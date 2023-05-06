5c36708d50fc4e94381746bf3764cd773fb89c8c
RocioMartinez <rym5411@gmail.com>
2023-05-05 21:59:28 -0400

    objects were created for lab6

--- app.js
+++ app.js
@@ -1,2 +1,108 @@
 'use strict';
 
+let hours = ['6am', '7am', '8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm', '7pm'];
+
+
+let seattle = {
+  name: 'Seattle',
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookieSale: 6.3,
+  customerNumber: 0,
+  randomNumCustomer: function(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  getNum: function(){
+    this.customerNumber = this.randomNumCustomer(23,65);
+  },
+  render: function(){
+    this.getNum();
+  },
+};
+
+
+let tokyo = {
+  name: 'Tokyo',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookieSale: 1.2,
+  customerNumber: 0,
+  randomNumCustomer: function(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  getNum: function(){
+    this.customerNumber = this.randomNumCustomer(3,24);
+  },
+  render: function(){
+    this.getNum();
+  },
+};
+
+let dubai = {
+  name: 'Dubai',
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookieSale: 3.7,
+  customerNumber: 0,
+  randomNumCustomer: function(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  getNum: function(){
+    this.customerNumber = this.randomNumCustomer(11,38);
+  },
+  render: function(){
+    this.getNum();
+  },
+};
+
+let paris = {
+  name: 'Paris',
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookieSale: 2.3,
+  customerNumber: 0,
+  randomNumCustomer: function(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  getNum: function(){
+    this.customerNumber = this.randomNumCustomer(20,38);
+  },
+  render: function(){
+    this.getNum();
+  },
+};
+
+let lima = {
+  name: 'Lima',
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookieSale: 4.6,
+  customerNumber: 0,
+  randomNumCustomer: function(min,max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  getNum: function(){
+    this.customerNumber = this.randomNumCustomer(2,16);
+  },
+  render: function(){
+    this.getNum();
+  },
+};
+
+
+seattle.render();
+console.log(seattle.customerNumber);
+
+tokyo.render();
+console.log(tokyo.customerNumber);
+
+dubai.render();
+console.log(dubai.customerNumber);
+
+paris.render();
+console.log(paris.customerNumber);
+
+lima.render();
+console.log(lima.customerNumber);
+
+
